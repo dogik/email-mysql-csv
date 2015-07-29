7f6ba664166f276c5c101d4ae04c0171fcd0d087
Sergey Dogotar <sergey.dogotar@gmail.com>
2015-07-29 15:32:32 -0400

    PHP mailer SMTP.

--- class.email-query-results-as-csv-file.php
+++ class.email-query-results-as-csv-file.php
@@ -25,263 +25,300 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+require_once 'vendor/autoload.php';
+
 class EmailQueryResultsAsCsv {
 
-  # MySQL Server
-  private $mySQL_server = '';
-  private $mySQL_server_port = '3306';
+	# MySQL Server
+	private $mySQL_server = '';
+	private $mySQL_server_port = '3306';
 
-  # MySQL Database Name
-  private $mySQL_database = '';
+	# MySQL Database Name
+	private $mySQL_database = '';
 
-  # MySQL Username
-  private $mySQL_user = '';
+	# MySQL Username
+	private $mySQL_user = '';
 
-  # MySQL Password
-  private $mySQL_password = '';
+	# MySQL Password
+	private $mySQL_password = '';
 
-  # MySQL Query
-  # something like "SELECT * FROM table_name"
-  private $mySQL_query = '';
+	# MySQL Query
+	# something like "SELECT * FROM table_name"
+	private $mySQL_query = '';
 
-  # CSV File Name
-  # filename for the attached file, use something like "mysql_results.csv"
-  private $csv_file_name = '';
-  
-  # Multiple File Data array
-  # File Name & reuseable elements + mySQL_query
-  private $arr_file_data = array();
-  
-  # CSV file reuseable elements
-  private $csv_contain = '';
-  private $csv_separate = '';
-  private $csv_end_row = '';
+	# CSV File Name
+	# filename for the attached file, use something like "mysql_results.csv"
+	private $csv_file_name = '';
 
-  # Email Message
-  # This is an HTML formatted message
-  private $email_html_msg = "<h1>MySQL Query Results as CSV Attachment</h1>
+	# Multiple File Data array
+	# File Name & reuseable elements + mySQL_query
+	private $arr_file_data = array();
+
+	# CSV file reuseable elements
+	private $csv_contain = '';
+	private $csv_separate = '';
+	private $csv_end_row = '';
+
+
+	private $smtp_host = '';
+	private $smtp_port = '587';
+	private $smtp_security = 'tls';
+	private $smtp_username = '';
+	private $smtp_password = '';
+
+	/**
+	 * @param string $smtp_host
+	 */
+	public function setSmtpHost($smtp_host)
+	{
+		$this->smtp_host = $smtp_host;
+	}
+
+	/**
+	 * @param string $smtp_port
+	 */
+	public function setSmtpPort($smtp_port)
+	{
+		$this->smtp_port = $smtp_port;
+	}
+
+	/**
+	 * @param string $smtp_security
+	 */
+	public function setSmtpSecurity($smtp_security)
+	{
+		$this->smtp_security = $smtp_security;
+	}
+
+	/**
+	 * @param string $smtp_username
+	 */
+	public function setSmtpUsername($smtp_username)
+	{
+		$this->smtp_username = $smtp_username;
+	}
+
+	/**
+	 * @param string $smtp_password
+	 */
+	public function setSmtpPassword($smtp_password)
+	{
+		$this->smtp_password = $smtp_password;
+	}
+
+	# Email Message
+	# This is an HTML formatted message
+	private $email_html_msg = "<h1>MySQL Query Results as CSV Attachment</h1>
   <p>This attachment can be opened with OpenOffice.org Calc, Google Docs, or Microsoft Excel.</p>";
-  
-  # used to output success messages to the screen
-  private $debugFlag = False;
-  
-  # -------------------------------------- 
-  #  Methods
-  # -------------------------------------- 
-    
-  # constructor
-  public function __construct($s, $d, $u, $p) {
-    $this->setDBinfo($s, $d, $u, $p);
-    $this->setCSVinfo();
-    $this->setCSVname();
-  }
-  
-  # destructor
-  public function __destruct() {
-    
-  }
-  
-  public function setDBinfo($s, $d, $u, $p) {
-    $this->mySQL_server = $s;
-    $this->mySQL_database = $d;
-    $this->mySQL_user = $u;
-    $this->mySQL_password = $p;
-  }
-  
-  public function setDBinfoServerPort($p) {
-    $this->mySQL_server_port = $p;
-  }
-  
-  public function setQuery($sql) {
-    $this->mySQL_query = $sql;
-  }
-  
-  public function setEmailMessage($msg) {
-    $this->email_html_msg = $msg;
-  }
-  
-  public function setCSVname($fn = "mysql_results.csv") {
-    $this->csv_file_name = $fn;
-  }
-  
-  public function setCSVinfo($c = '"', $s = ",", $er = "\n") {
-    $this->csv_contain = $c;
-    $this->csv_separate = $s;
-    $this->csv_end_row = $er;
-  }
-  
-  public function setMultiFile($fn, $sql) {
-    $this->arr_file_data[] = array("csv_file_name" => $fn, 
-      "mySQL_query" => $sql,
-      "csv_contain" => $this->csv_contain,
-      "csv_separate" => $this->csv_separate,
-      "csv_end_row" => $this->csv_end_row);
-  }
-  
-  public function debugMode($bool) {
-    $this->debugFlag = $bool;
-  }
-  
-  public function sendEmail($email_from, $email_to, $email_subject) {
-    # check to see if the array for file info and queries has data if not add the single file data
-    if(!isset($this->arr_file_data[0]["csv_file_name"])) {
-      $this->arr_file_data[0] = array("csv_file_name" => $this->csv_file_name, 
-        "mySQL_query" => $this->mySQL_query,
-        "csv_contain" => $this->csv_contain,
-        "csv_separate" => $this->csv_separate,
-        "csv_end_row" => $this->csv_end_row);
-    }
 
-    # -------------------------------------- 
-    #   CONNECT TO MYSQL DATABASE
-    $mysqli = new mysqli($this->mySQL_server, $this->mySQL_user, $this->mySQL_password, $this->mySQL_database, $this->mySQL_server_port);
-    if ($mysqli->connect_errno) {
-         die('ERROR: Could not connect to MySQL server: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
-    }
-    if ($this->debugFlag) {
-      echo "Step 1: Connected to MySQL server successfully. \n\n";
-    }
-    
-    /*
-    -------------------------------------- 
-        SEND EMAIL WITH ATTACHMENT
-        Below you will notice I'm using complex variable parsing {curly braces surrounding variable}
-          For more information see: PHP docs for String Variable Parsing using Complex (curly) syntax
-          http://www.php.net/manual/en/language.types.string.php#language.types.string.parsing.complex
-    */
+	# used to output success messages to the screen
+	private $debugFlag = false;
 
-    # start setting up the email header
-    $headers = "From: ".$email_from;
+	# --------------------------------------
+	#  Methods
+	# --------------------------------------
 
-    # create boundary string
-    # boundary string must be unique using MD5 to generate a pseudo random hash
-    $random_hash = md5(date('r', time())); 
-    $mime_boundary = "==Multipart_Boundary_x{$random_hash}x";
+	# constructor
+	public function __construct($s, $d, $u, $p)
+	{
+		$this->setDBinfo($s, $d, $u, $p);
+		$this->setCSVinfo();
+		$this->setCSVname();
+	}
 
-    # set email header as a multipart/mixed message
-    # this allows the sending of an attachment combined with the HTML message
-    $headers .= "\nMIME-Version: 1.0\n" .
-    "Content-Type: multipart/mixed;\n" .
-    " boundary=\"{$mime_boundary}\"";
+	# destructor
+	public function __destruct()
+	{
 
-    # multipart boundary for the HTML message
-    $email_message = "This is a multi-part message in MIME format.\n\n" .
-    "--{$mime_boundary}\n" .
-    "Content-Type:text/html; charset=\"UTF-8\"\n" .
-    "Content-Transfer-Encoding: 7bit\n\n" .
-    $this->email_html_msg . "\n\n";
-    
-    # count how many files were created
-    $files_attached_cnt = 0;
-    foreach ($this->arr_file_data AS $file_data) {
-      
-      # build the CSV file
-      $csv_file = $this->buildCSV($file_data, $mysqli);
-      
-      if ($csv_file != "") {
-        $files_attached_cnt += 1;
-      
-        # encode CSV file with MIME base64
-        # required for sending it as an email attachment
-        $data = chunk_split(base64_encode($csv_file)); 
-        
-        # multipart boundary for the email attachment
-        $email_message .= "--{$mime_boundary}\n";
-        
-        # attach the file
-        $email_message .= "Content-Type: application/octet-stream;\n" .
-        " name=\"{$file_data['csv_file_name']}\"\n" .
-        "Content-Disposition: attachment;\n" .
-        " filename=\"{$file_data['csv_file_name']}\"\n" .
-        "Content-Transfer-Encoding: base64\n\n" .
-        $data . "\n\n";
-      }
-    }
-    # end the multipart message with this mime boundary, notice the ending --
-    $email_message .= "--{$mime_boundary}--\n";
-    
-    if ($files_attached_cnt > 0) {
-      # try to send the email and verify the results
+	}
 
-      $sendit = @mail($email_to, $email_subject, $email_message, $headers);
-      if(!$sendit) {
-        die("ERROR: The Email could not be sent.");
-      }
+	public function setDBinfo($s, $d, $u, $p)
+	{
+		$this->mySQL_server = $s;
+		$this->mySQL_database = $d;
+		$this->mySQL_user = $u;
+		$this->mySQL_password = $p;
+	}
 
-      if ($this->debugFlag) {
-        echo "Step 4: Email sent with attachment. \n\n";
-      }
-    } else {
-      if ($this->debugFlag) {
-        echo "Step 4: No data found for query. Email NOT sent. \n\n";
-      }
-    }
+	public function setDBinfoServerPort($p)
+	{
+		$this->mySQL_server_port = $p;
+	}
 
-    # close the link to the MySQL database
-    //$mysqli->close();
-    
-    # reset the attachment array so the object can be used anew
-    $this->arr_file_data = array();
+	public function setQuery($sql)
+	{
+		$this->mySQL_query = $sql;
+	}
 
-    if ($this->debugFlag) {
-      echo "FINISHED.";
-    }
-  }
-  
-  private function buildCSV($file_data, $mysqli) {
-    # container to hold the CSV file as it's built
-    $csv_file = "";
-    
-    # run the MySQL query and check to see if results were returned
-    $result = $mysqli->query($file_data["mySQL_query"]);
-    if (!$result) {
-      die("ERROR: Invalid query \n MySQL error: (" . $mysqli->errno . ")" . $mysqli->error . "\n Your query: " . $this->mySQL_query);
-    }
-    
-    # only return a non blank data set with query returns at least one record
-    if ($result->num_rows > 0) {
-      if ($this->debugFlag) {
-        echo "Step 2 (repeats for each attachment): MySQL query ran successfully. \n\n";
-      }
-      
-      # store the number of columns and field data from the results
-      $columns = $mysqli->field_count;
-      $column_data = $result->fetch_fields();
-      
-      # Build a header row using the mysql field names
-      $header_row = '';
-      $i = 0;
-      foreach ($column_data as $col) {
-      //for ($i = 0; $i < $columns; $i++) {
-        $column_title = $file_data["csv_contain"] . stripslashes($col->name) . $file_data["csv_contain"];
-        $column_title .= ($i < $columns-1) ? $file_data["csv_separate"] : ''; #the last column does not have the column separator
-        $header_row .= $column_title;
-        $i++;
-      }
-      $csv_file .= $header_row . $file_data["csv_end_row"]; # add header row to CSV file
-      
-      # Build the data rows by walking through the results array one row at a time
-      $data_rows = '';
-      while ($row = $result->fetch_array(MYSQLI_NUM)) {
-        for ($i = 0; $i < $columns; $i++) {
-          # clean up the data; strip slashes; replace double quotes with two single quotes
-          $data_rows .= $file_data["csv_contain"] . preg_replace('/'.$file_data["csv_contain"].'/', $file_data["csv_contain"].$file_data["csv_contain"], stripslashes($row[$i])) . $file_data["csv_contain"];
-          $data_rows .= ($i < $columns-1) ? $file_data["csv_separate"] : '';
-        }
-        $data_rows .= $this->csv_end_row; # add data row to CSV file
-      }
-      $csv_file .= $data_rows; # add the data rows to CSV file
-      
-      if ($this->debugFlag) {
-        echo "Step 3 (repeats for each attachment): CSV file built. \n\n";
-      }
-    }  else {
-       echo "Step 2 (repeats for each attachment): MySQL query ran successfully \n\n";
-       echo "Step 3 (repeats for each attachment): NO results were returned for this query. No file will be sent for the following query: \n " . $this->mySQL_query ." \n\n";
-    }
-    
-    # Return the completed file
-    return $csv_file;
-  }
+	public function setEmailMessage($msg)
+	{
+		$this->email_html_msg = $msg;
+	}
+
+	public function setCSVname($fn = "mysql_results.csv")
+	{
+		$this->csv_file_name = $fn;
+	}
+
+	public function setCSVinfo($c = '"', $s = ",", $er = "\n")
+	{
+		$this->csv_contain = $c;
+		$this->csv_separate = $s;
+		$this->csv_end_row = $er;
+	}
+
+	public function setMultiFile($fn, $sql)
+	{
+		$this->arr_file_data[] = array("csv_file_name" => $fn, "mySQL_query" => $sql, "csv_contain" => $this->csv_contain, "csv_separate" => $this->csv_separate, "csv_end_row" => $this->csv_end_row);
+	}
+
+	public function debugMode($bool)
+	{
+		$this->debugFlag = $bool;
+	}
+
+	public function sendEmail($email_from, $email_to, $email_subject, $email_body, $filename)
+	{
+		# check to see if the array for file info and queries has data if not add the single file data
+		if (!isset($this->arr_file_data[0]["csv_file_name"]))
+		{
+			$this->arr_file_data[0] = array("csv_file_name" => $this->csv_file_name, "mySQL_query" => $this->mySQL_query, "csv_contain" => $this->csv_contain, "csv_separate" => $this->csv_separate, "csv_end_row" => $this->csv_end_row);
+		}
+
+		# --------------------------------------
+		#   CONNECT TO MYSQL DATABASE
+		$mysqli = new mysqli($this->mySQL_server, $this->mySQL_user, $this->mySQL_password, $this->mySQL_database, $this->mySQL_server_port);
+		if ($mysqli->connect_errno)
+		{
+			die('ERROR: Could not connect to MySQL server: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+		}
+		if ($this->debugFlag)
+		{
+			echo "Step 1: Connected to MySQL server successfully. \n\n";
+		}
+
+		foreach ($this->arr_file_data AS $file_data)
+		{
+			$csv_file = $this->buildCSV($file_data, $mysqli);
+		}
+
+		file_put_contents($filename, $csv_file);
+
+		$mail = new PHPMailer;
+
+		$mail->isSMTP();                                      // Set mailer to use SMTP
+		$mail->Host = $this->smtp_host;  // Specify main and backup SMTP servers
+		$mail->SMTPAuth = true;                               // Enable SMTP authentication
+		$mail->Username = $this->smtp_username;                 // SMTP username
+		$mail->Password = $this->smtp_password;                           // SMTP password
+		$mail->SMTPSecure = $this->smtp_security;                            // Enable TLS encryption, `ssl` also accepted
+		$mail->Port = $this->smtp_port;                                    // TCP port to connect to
+
+		$mail->From = $email_from;
+		$mail->FromName = 'Mailer';
+
+		if (!is_array($email_to))
+		{
+			$email_to = [$email_to];
+		}
+
+		foreach ($email_to as $to)
+		{
+			$mail->addAddress($to);
+		}
+
+		$mail->addReplyTo($email_from);
+
+		$mail->addAttachment($filename);         // Add attachments
+		$mail->isHTML(true);                                  // Set email format to HTML
+
+		$mail->Subject = $email_subject;
+		$mail->Body = $email_body;
+
+		if (!$mail->send())
+		{
+			echo 'Message could not be sent.';
+			echo 'Mailer Error: ' . $mail->ErrorInfo;
+		}
+		else
+		{
+			echo 'Message has been sent';
+		}
+
+		# reset the attachment array so the object can be used anew
+		$this->arr_file_data = array();
+
+		if ($this->debugFlag)
+		{
+			echo "FINISHED.";
+		}
+	}
+
+	private function buildCSV($file_data, $mysqli)
+	{
+		# container to hold the CSV file as it's built
+		$csv_file = "";
+
+		# run the MySQL query and check to see if results were returned
+		$result = $mysqli->query($file_data["mySQL_query"]);
+		if (!$result)
+		{
+			die("ERROR: Invalid query \n MySQL error: (" . $mysqli->errno . ")" . $mysqli->error . "\n Your query: " . $this->mySQL_query);
+		}
+
+		# only return a non blank data set with query returns at least one record
+		if ($result->num_rows > 0)
+		{
+			if ($this->debugFlag)
+			{
+				echo "Step 2 (repeats for each attachment): MySQL query ran successfully. \n\n";
+			}
+
+			# store the number of columns and field data from the results
+			$columns = $mysqli->field_count;
+			$column_data = $result->fetch_fields();
+
+			# Build a header row using the mysql field names
+			$header_row = '';
+			$i = 0;
+			foreach ($column_data as $col)
+			{
+				//for ($i = 0; $i < $columns; $i++) {
+				$column_title = $file_data["csv_contain"] . stripslashes($col->name) . $file_data["csv_contain"];
+				$column_title .= ($i < $columns - 1) ? $file_data["csv_separate"] : ''; #the last column does not have the column separator
+				$header_row .= $column_title;
+				$i++;
+			}
+			$csv_file .= $header_row . $file_data["csv_end_row"]; # add header row to CSV file
+
+			# Build the data rows by walking through the results array one row at a time
+			$data_rows = '';
+			while ($row = $result->fetch_array(MYSQLI_NUM))
+			{
+				for ($i = 0; $i < $columns; $i++)
+				{
+					# clean up the data; strip slashes; replace double quotes with two single quotes
+					$data_rows .= $file_data["csv_contain"] . preg_replace('/' . $file_data["csv_contain"] . '/', $file_data["csv_contain"] . $file_data["csv_contain"], stripslashes($row[$i])) . $file_data["csv_contain"];
+					$data_rows .= ($i < $columns - 1) ? $file_data["csv_separate"] : '';
+				}
+				$data_rows .= $this->csv_end_row; # add data row to CSV file
+			}
+			$csv_file .= $data_rows; # add the data rows to CSV file
+
+			if ($this->debugFlag)
+			{
+				echo "Step 3 (repeats for each attachment): CSV file built. \n\n";
+			}
+		}
+		else
+		{
+			echo "Step 2 (repeats for each attachment): MySQL query ran successfully \n\n";
+			echo "Step 3 (repeats for each attachment): NO results were returned for this query. No file will be sent for the following query: \n " . $this->mySQL_query . " \n\n";
+		}
+
+		# Return the completed file
+		return $csv_file;
+	}
 }
+
 ?>
--- example-min.php
+++ example-min.php
@@ -2,7 +2,15 @@
 # minimum code use example
 # see example.php for more options that you can use, and for trouble shooting tips
 require 'class.email-query-results-as-csv-file.php';
-$emailCSV = new EmailQueryResultsAsCsv('localhost','database_name','username','password');
-$emailCSV->setQuery("SELECT * FROM table_name WHERE column = 'value'");
-$emailCSV->sendEmail("sender@website.com","receiver@website.com","MySQL Query Results as CSV Attachment");
+
+$emailCSV = new EmailQueryResultsAsCsv('localhost','databae','user','password');
+
+$emailCSV->setSmtpHost('');
+$emailCSV->setSmtpPort('465');
+$emailCSV->setSmtpUsername('');
+$emailCSV->setSmtpPassword('');
+$emailCSV->setSmtpSecurity('ssl');
+
+$emailCSV->setQuery("SELECT * FROM table");
+$emailCSV->sendEmail("sender@example.com","recipient@example.com","MySQL Query Results as CSV Attachment", 'Email body', 'file.csv');
 ?>
